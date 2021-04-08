14c5b82a071e0b67c6619c82c49bb946de5350bd
George Jacob <georgekunchattil@gmail.com>
2021-04-09 00:34:07 +0530

    activate-deactivate plugin

--- wp-content/plugins/activate-deactivate.php
+++ wp-content/plugins/activate-deactivate.php
@@ -9,3 +9,11 @@ Version: 1.0
 */
 
 
+//do stuff on activation
+function myplugin_onactivation() {
+	if(!current_user_can('activate_plugins')) return;
+
+	add_option('myplugin_posts_per_page', 10); //10 posts shown per page
+	add_option('myplugin_show_welcome_page', true); //show welcome message
+}
+register_activation_hook(_FILE_, 'myplugin_onactivation'); // _FILE _ --> means path to current file
