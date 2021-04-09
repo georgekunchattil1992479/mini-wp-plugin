2b39d7c7166623b1ce43d9274b0cbff105fd5f20
George Jacob <georgekunchattil@gmail.com>
2021-04-09 09:58:21 +0530

    activate, deactivate & uninstall hooks

--- wp-content/plugins/activate-deactivate.php
+++ wp-content/plugins/activate-deactivate.php
@@ -9,7 +9,7 @@ Version: 1.0
 */
 
 
-//do stuff on activation
+//do stuff on activation. Activate hook function used for activate plugin
 function myplugin_onactivation() {
 	if(!current_user_can('activate_plugins')) return;
 
@@ -17,3 +17,32 @@ function myplugin_onactivation() {
 	add_option('myplugin_show_welcome_page', true); //show welcome message
 }
 register_activation_hook(_FILE_, 'myplugin_onactivation'); // _FILE _ --> means path to current file
+
+
+
+// do stuff on deactivation. deactivate hook function used for deactivate the plugin
+function myplugin_on_deactivation() {
+
+	// wp_die('The plugin has been deactivated!');
+
+	if (!current_user_can('activate_plugins')) return; 
+
+    //this function used like custom post types. flush_rewrite_rules() removes functionality while clicking deactivate option
+	flush_rewrite_rules(); 
+}
+
+register_deactivation_hook(_FILE_, 'myplugin_on_deactivation');
+
+
+
+//do stuff on uninstall. uninstall hook function used to uninstall plugin
+function myplugin_on_uninstall() { 
+
+	if (!current_user_can('activate_plugins'))  return;
+
+	delete_option('myplugin_posts_per_page', 10);
+	delete_option('myplugin_show_welcome_page', true);
+}
+
+//remove options and functionality in plugin when click "Delete" button of that WordPress plugin
+register_uninstall_hook(_FILE_, 'myplugin_on_uninstall'); 
